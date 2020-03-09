5caa82269ff4a71b347ab657680fe99140ef5ab5
Ivan Huayraña <ivanhuay@gmail.com>
2020-03-09 08:44:14 -0300

    markdown preview working

--- src/components/markdown-editor/index.js
+++ src/components/markdown-editor/index.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import * as Showdown from 'showdown';
-import { NavArea } from '../base';
+import { NavArea, Button } from '../base';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
-// import 'prismjs/components/prism-javascript';
-// import 'prismjs/components/prism-markup-templating';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-markdown';
 
@@ -20,19 +18,36 @@ const converter = new Showdown.Converter({
 
 const MarkdownEditor = () => {
   const [value, setValue] = useState('**Hello world!!!**');
-  console.log(Object.keys(languages));
+  const [previewEnabled, setPreviewEnabled] = useState(false);
+  const [preview, setPreview] = useState('');
+  const handlePreview = () => {
+    setPreviewEnabled(prev => !prev);
+    const html = converter.makeHtml(value);
+    setPreview(html);
+  };
+
   return (
     <div className={styles.editor}>
-      <NavArea title="Text Editor" />
-      <Editor
-        value={value}
-        onValueChange={newCode => setValue(newCode)}
-        highlight={textCode =>
-          highlight(textCode, languages.markdown, 'markdown')
-        }
-        padding={10}
-        className={styles.editor}
-      />
+      <NavArea title="Markdown Editor">
+        <Button value="preview" onClick={handlePreview} />
+      </NavArea>
+      {!previewEnabled && (
+        <Editor
+          value={value}
+          onValueChange={newCode => setValue(newCode)}
+          highlight={textCode =>
+            highlight(textCode, languages.markdown, 'markdown')
+          }
+          padding={10}
+          className={styles.editor}
+        />
+      )}
+      {previewEnabled && (
+        <div
+          className={styles.markdown}
+          dangerouslySetInnerHTML={{ __html: preview }}
+        />
+      )}
     </div>
   );
 };
